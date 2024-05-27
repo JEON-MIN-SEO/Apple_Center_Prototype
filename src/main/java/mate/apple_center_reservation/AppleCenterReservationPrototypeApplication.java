38738ef39b27983401493a03ac4cbb8ee299e4c7
jmsparrowk <jmsparrowk@naver.com>
2024-05-27 14:44:32 +0900

    5/27

---
+++ src/main/java/mate/apple_center_reservation/AppleCenterReservationPrototypeApplication.java
@@ -0,0 +1,13 @@
+package mate.apple_center_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleCenterReservationPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppleCenterReservationPrototypeApplication.class, args);
+	}
+
+}
